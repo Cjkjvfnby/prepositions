0aea66748335f8dac5f89c8a3b3763d5e4267f41
Andrey <Cjkjvfnby@gmail.com>
2023-11-30 22:54:29 +0100

    Review ecamples

--- prepositions/_prepositions.py
+++ prepositions/_prepositions.py
@@ -9,10 +9,11 @@ TODO replace with data markup language.
 """
 
 from collections.abc import Iterable, Sequence
-from typing import NamedTuple, NewType, TypedDict
+from typing import NamedTuple, NewType
 
-RuExample = str
-FrExample = str
+Example = str
+RuExample = Example
+FrExample = Example
 
 
 Lang = NewType("Lang", str)
@@ -39,120 +40,31 @@ class Relation(NamedTuple):
     examples: Sequence[tuple[RuExample, FrExample]]
 
 
-class _DataItem(TypedDict):
-    ru: str
-    fr: str
-    examples: dict[RuExample, FrExample]
+data: dict[RuExample, FrExample] = {
+    "Он сидит *в* кресле": "Il est assis *dans* le fauteuil",
+    "Я *в* Европе": "Je suis *en* Europe",
+    "Мы собираемся *в* Ниццу": "Nous allons *à* Nice",
+    "бокал *для* вина": "un verre *à* vin",
+    "*до* завтра": "*à* demain",
+    "Я сделал это *за* пять минут": "Je l’ai fait *en* cinq minutes",
+    "пить *из* стакана": "boire *dans* un verre",
+    "*из* города": "*de* la ville",
+    "Сделанно *из* дерева": "Fabriqué *en* bois",
+    "Я иду *к* Пьеру": "Je vais *chez* Pierre",
+    "Пойду *к* окну": "J'irai *à* la fenêtre",
+    "Он вёл себя *как* тиран": "Il a agi *en* tyran",
+    "Он *на* работе": "Il est *chez* son travail",
+    "Он предпочитает путешествовать *на* поезде": "Il préfère voyager *en* train",
+    "*на* пляже": "*à* la plage",
+    "фильм *о* войне": "un film *de* guerre",
+    "Мы были в 5 километрах *от* пляжа": "Nous sommes à 5 km *de* la plage",
+    "с понедельника *по* субботу": "du lundi *au* samedi",
+    "стакан *с* вином": "un verre *de* vin",
+    "*с* понедельника по субботу": "*du* lundi au samedi",
+    "обувь *с* высоким каблуком": "chaussures *à* talon haut",
+    "Я *у* художника": "Je suis *chez* un artiste",
+}
 
-
-data: list[_DataItem] = [
-    {
-        "ru": "в",
-        "fr": "dans",
-        "examples": {"Он сидит *в* кресле": "Il est assis *dans* le fauteuil"},
-    },
-    {"ru": "в", "fr": "en", "examples": {"Я *в* Европе": "Je suis *en* Europe"}},
-    {
-        "ru": "в",
-        "fr": "à",
-        "examples": {"Мы собираемся *в* Ниццу": "Nous allons *à* Nice"},
-    },
-    {"ru": "для", "fr": "à", "examples": {"бокал *для* вина": "un verre *à* vin"}},
-    {"ru": "до", "fr": "à", "examples": {"*до* завтра": "*à* demain"}},
-    {
-        "ru": "за",
-        "fr": "en",
-        "examples": {"Я сделал это *за* пять минут": "Je l’ai fait *en* cinq minutes"},
-    },
-    {
-        "ru": "из",
-        "fr": "dans",
-        "examples": {"пить *из* стакана": "boire *dans* un verre"},
-    },
-    {"ru": "из", "fr": "de", "examples": {"*из* города": "*de* la ville"}},
-    {
-        "ru": "из",
-        "fr": "en",
-        "examples": {"Сделанно *из* дерева": "Fabriqué *en* bois"},
-    },
-    {"ru": "к", "fr": "chez", "examples": {"Я иду *к* Пьеру": "Je vais *chez* Pierre"}},
-    {"ru": "к", "fr": "à", "examples": {"Пойду *к* окну": "J'irai à la fenêtre"}},
-    {
-        "ru": "как",
-        "fr": "en",
-        "examples": {"Он вёл себя *как* тиран": "Il a agi *en* tyran"},
-    },
-    {
-        "ru": "на",
-        "fr": "chez",
-        "examples": {"Он *на* работе": "Il est *chez* son travail"},
-    },
-    {
-        "ru": "на",
-        "fr": "en",
-        "examples": {
-            "Он предпочитает путешествовать *на* поезде": "Il préfère voyager *en* train"
-        },
-    },
-    {"ru": "на", "fr": "à", "examples": {"*на* пляже": "*à* la plage"}},
-    {"ru": "о", "fr": "de", "examples": {"фильм *о* войне": "un film *de* guerre"}},
-    {
-        "ru": "от",
-        "fr": "de",
-        "examples": {
-            "Мы были в 5 километрах *от* пляжа": "Nous sommes à 5 km *de* la plage"
-        },
-    },
-    {
-        "ru": "по",
-        "fr": "à",
-        "examples": {"с понедельника *по* субботу": "du lundi *au* samedi"},
-    },
-    {
-        "ru": "с",
-        "fr": "de",
-        "examples": {
-            "стакан *с* вином": "un verre *de* vin",
-            "*с* понедельника по субботу": "*du* lundi au samedi",
-        },
-    },
-    {
-        "ru": "с",
-        "fr": "à",
-        "examples": {"обувь *с* высоким каблуком": "chaussures *à* talon haut"},
-    },
-    {
-        "ru": "у",
-        "fr": "chez",
-        "examples": {"Я *у* художника": "Je suis *chez* un artiste"},
-    },
-]
-
-
-"""
-Chat GPT promt example
-
-
-Для французского предлога de найди мне варианты использования, которые переводятся на русский предложением с предлогом.
-В переводе обязательно должна быть часть речи предлог, если его нет, то такой пример не подходит.
-
-Нужно найти все возможные русские предлоги которые переводятся с заданного французского.
-Пример должны быть отличными друг от друга, один пример для каждого русского предлога.
-
-Результат должен быть оформлен в виде списка на языке Python с форматированием для удобства чтения.
-Все элементы должны иметь хвостовую запятую запятую.
-
-Каждый элемент списка это словарь с тремя ключами.
-
-"ru" со значением русского предлога из перевода
-"fr" со значенем французского предлога
-"examples" словарь с примерами.  Где русский пример является ключом, а французский перевод значением.
-Каждая пара ключ значение должна быть на новой строке.
-
-В примерах нужно убрать точки и предлог обрамить звёздочками.
-
-
-"""
 
 _synonyms = {
     "de": ("du",),
@@ -160,18 +72,34 @@ _synonyms = {
 }
 
 
+_synonyms_conversion = {}
+for k, vals in _synonyms.items():
+    for syn in vals:
+        _synonyms_conversion[syn] = k
+
+
+def _get_prep(text: Example) -> str:
+    assert text.count("*") == 2, text  # noqa: PLR2004
+
+    prep = text.split("*")[1]
+    return _synonyms_conversion.get(prep, prep)
+
+
 def get_db() -> tuple[list[Preposition], list[Relation]]:
     prepositions: set[Preposition] = set()
     relations: dict[
         tuple[Preposition, Preposition], list[tuple[RuExample, FrExample]]
     ] = {}
-    for case in data:
-        ru_prep = Preposition(RU, case["ru"])
-        fr_prep = Preposition(FR, case["fr"], synonyms=_synonyms.get(case["fr"], ()))
+    for ru_example, fr_example in data.items():
+        ru = _get_prep(ru_example)
+        fr = _get_prep(fr_example)
+
+        ru_prep = Preposition(RU, ru, synonyms=_synonyms.get(fr, ()))
+        fr_prep = Preposition(FR, fr, synonyms=_synonyms.get(fr, ()))
         prepositions.add(ru_prep)
         prepositions.add(fr_prep)
 
-        relations.setdefault((ru_prep, fr_prep), []).extend(case["examples"].items())
+        relations.setdefault((ru_prep, fr_prep), []).append((ru_example, fr_example))
 
     return sorted(prepositions, key=lambda p: (p.lang != RU, p.preposition)), [
         Relation(ru, fr, examples) for (ru, fr), examples in relations.items()
@@ -185,24 +113,3 @@ if __name__ == "__main__":
     print("=" * 10)  # noqa: T201
     for r in rels:
         print(r)  # noqa: T201
-
-    # Reformat data in order
-    data = sorted(
-        data, key=lambda item: (item["ru"], item["fr"], tuple(item["examples"].keys()))
-    )
-
-    deduplicated: dict[tuple[str, str], dict[RuExample, FrExample]] = {}
-    for item in data:
-        ru, fr, examples = item["ru"], item["fr"], item["examples"]
-        deduplicated.setdefault((ru, fr), {}).update(examples)
-
-    data = [
-        {"ru": ru, "fr": fr, "examples": examples}
-        for (ru, fr), examples in deduplicated.items()
-    ]
-
-    data = sorted(
-        data, key=lambda item: (item["ru"], item["fr"], tuple(item["examples"].keys()))
-    )
-
-    print(data)  # noqa: T201
